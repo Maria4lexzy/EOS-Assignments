283c772516cf794fbe44546636fba71b43ac276a
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2020-10-29 02:57:05 +0100

    Add project files.

---
+++ EOS Assignments.cpp
@@ -0,0 +1,42 @@
+// EOS Assignments.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string pathLED = "sys/class/leds/beaglebone:green:usr";
+	int numberLED;
+	int ledValue;
+		 string ledMode;
+	cout << "enter the LED number you would like to turn on";
+	cin >> ledValue;
+	cout << "enter a mode for the LED";
+	cin >> ledMode;
+	
+	if (ledValue!=NULL && !(ledMode.empty)) {
+		cout << "you entered: " << ledValue + " " + ledMode;
+
+	}
+	else
+	{
+		cout << "enter the LED number you would like to turn on & the mode you would like to activate";
+
+	}
+
+
+	
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ assignment2.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+using namespace std;
+using std::string;
+/*
+Make a C/C++ function/method that can adjust the light intensity to a level between 0 and 100 % (visually graded),
+given by a parameter. e.g. turn on max. intensity: pwm_light(100);
+• Write a simple program that use the function to demonstrate it works.
+It must take one command line parameter to set light intensity; e.g. set_light 50 (to set light intensity to 50%)
+• The program must check whether the port and PWM channel are initialized, otherwise do this first.
+• Use one of the Beaglebone’s PWM channels for output.
+*/
+string pwmLEDPath = "sys/class/pwm/pwmchip1";
+string pwmLEDState = "cat/sys/ devices/platform/ocp/ocp:P9_21_pinmux/state";
+
+int main()
+{
+
+	
+	int ledValue;
+	string ledMode;
+	cout << "enter the LED number you would like to turn on";
+	cin >> ledValue;
+	cout << "enter a mode for the LED";
+	cin >> ledMode;
+
+	cout << "you entered: " << ledValue;
+	return 0;
+}
+void pwm_light(int intensity) {
+
+	//check if pin is set for pwm if it is not set it for pwm
+	if (pwmLEDState != "pwm")
+		{
+
+		Config - pin P9_21 pwm
+		}
+
+}
+// Run program: 
